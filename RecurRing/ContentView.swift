70cfe905ad2c3ba6e2274776ab44cd83cc038d71
Luke Faupel <lukefaupel@gmail.com>
2024-03-02 13:33:56 -0600

    Initial Commit

---
+++ RecurRing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecurRing
+//
+//  Created by Luke Faupel on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecurRing/RecurRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecurRingApp.swift
+//  RecurRing
+//
+//  Created by Luke Faupel on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct RecurRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
